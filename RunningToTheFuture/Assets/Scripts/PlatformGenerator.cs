f1f32e8b36234257c27d8ed0e0222c278ad25898
Cristina <cristina.personal.projects@gmail.com>
2021-10-28 17:47:37 +0200

    UPDATE: platforms y positoin is limited between min and max height limits

--- RunningToTheFuture/Assets/Scripts/PlatformGenerator.cs
+++ RunningToTheFuture/Assets/Scripts/PlatformGenerator.cs
@@ -18,6 +18,15 @@ public class PlatformGenerator : MonoBehaviour
     private float[] platformWidths;
     public ObjectPooler[] objectPools;
 
+
+    private float minHeight; //starting point of platforms
+    public Transform maxHeightPoint;
+    private float maxHeight;
+    public float maxHeightChange; //maximo salto para que no aparezcan muy arriba las plataformas
+    private float heightChange;
+
+     
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +37,9 @@ public class PlatformGenerator : MonoBehaviour
             platformWidths[i] = objectPools[i].pooledObject.GetComponent<BoxCollider2D>().size.x;
             Console.WriteLine("platformWidth: " + platformWidths[i]);
         }
+
+        minHeight = transform.position.y;
+        maxHeight = maxHeightPoint.position.y;
     }
 
     // Update is called once per frame
@@ -37,7 +49,23 @@ public class PlatformGenerator : MonoBehaviour
         {
             distanceBetween = UnityEngine.Random.Range(distanceBetweenMin, distanceBetweenMax);
             platformSelector = UnityEngine.Random.Range(0, objectPools.Length);
-            transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2) + distanceBetween, transform.position.y, transform.position.z);
+
+            //poner las plataformas a la altura
+            heightChange = transform.position.y + UnityEngine.Random.Range(maxHeightChange, -maxHeightChange); // platforms currently + random value
+                                                                                                               //heightChange = transform.position.y + 2; // platforms currently + random value
+
+            // que no aparezcan fuera de los limites
+            if (heightChange > maxHeight)
+            {
+                heightChange = maxHeight;
+            } else if (heightChange < minHeight) 
+            {
+                heightChange = minHeight;
+
+            }
+            //antes de que se muevan logicamnete
+            //transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2) + distanceBetween, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2) + distanceBetween, heightChange, transform.position.z);
 
             // Instantiate(/*platform*/ objectPools[platformSelector].pooledObject, transform.position, transform.rotation);
 
