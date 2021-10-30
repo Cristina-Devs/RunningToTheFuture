7f7118e987b39a4faf975802fdafebe5a5c37b02
Cristina <cristina.personal.projects@gmail.com>
2021-10-30 13:45:03 +0200

    ADD: simple pause menu option, reusing death pannel, and with a simple express blue button

---
+++ RunningToTheFuture/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public string mainMenuLevel;
+    public GameObject pauseMenu;
+
+    public void PauseGame()
+    {
+        //SceneManager.LoadScene(pauseMenuLevel);
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+        FindObjectOfType<GameManager>().Reset();
+    }
+
+    public void QuitToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(mainMenuLevel);
+    }
+}
