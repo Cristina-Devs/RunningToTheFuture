c59261f045a8576c842ee98a4d7a61e89e205f9c
Cristina <cristina.personal.projects@gmail.com>
2021-10-30 12:34:07 +0200

    ADD: added new Main Menu Scene, to choose to Start/ Quit game, and also updated the DeathMenu to choose if return back to main menu or simply restart the current scene

--- RunningToTheFuture/Assets/Scripts/DeathMenu.cs
+++ RunningToTheFuture/Assets/Scripts/DeathMenu.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathMenu : MonoBehaviour
 {
-    //public string mainMenuLevel;
+    public string mainMenuLevel;
     public void RestartGame()
     {
         FindObjectOfType<GameManager>().Reset();
 
     }
 
-
+    public void QuitToMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuLevel);
+    }
 }
