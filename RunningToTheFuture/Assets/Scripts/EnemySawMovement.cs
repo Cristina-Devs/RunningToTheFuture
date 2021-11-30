a894de4afd70b3f45bc7a9965fb7858f6aa7dfa7
Cristina <cristina.personal.projects@gmail.com>
2021-12-01 00:47:31 +0100

    UPDATE - enemy movement first part, with saw prefab and script

--- RunningToTheFuture/Assets/Scripts/EnemySawMovement.cs
+++ RunningToTheFuture/Assets/Scripts/EnemySawMovement.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class EnemySawMovement : MonoBehaviour
 {
+    /*public float movementDistance;
+    public float speed;
+    private bool movingLeft;
+    private float leftEdge;
+    private float rightEdge;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        leftEdge = transform.position.x - movementDistance;
+        rightEdge = transform.position.x + movementDistance;
+        Debug.Log("Starttttt leftEdge: " + leftEdge);
+        Debug.Log("Starttttt rightEdge: " + rightEdge);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (movingLeft)
+        {
+            goMoveLeft();
+        }
+        else 
+        {
+            goMoveRight();
+        }
         
     }
+
+    void goMoveLeft() 
+    {
+        Debug.Log("-- goMoveLeft ? -- transform.position.x > leftEdge: " + transform.position.x);
+        if (transform.position.x > leftEdge)
+        {
+            Debug.Log("-- goMoveLeft -- YES");
+            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            Debug.Log("-- goMoveLeft -- NOUP");
+            movingLeft = false;
+        }
+    }
+
+    void goMoveRight()
+    {
+        Debug.Log("-- goMoveRight ? -- transform.position.x < rightEdge: " + transform.position.x);
+        if (transform.position.x < rightEdge)
+        {
+            Debug.Log("-- goMoveRight -- YES");
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            Debug.Log("-- goMoveRight -- NOUP");
+            movingLeft = true;
+        }
+    }*/
 }
