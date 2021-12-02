2326358b84445d7f2b3af918bb0d931eff2238b2
Cristina <cristina.personal.projects@gmail.com>
2021-12-02 02:54:07 +0100

    added new buttons and fixed the old ones with extra margin

--- RunningToTheFuture/Assets/Scripts/DeathMenu.cs
+++ RunningToTheFuture/Assets/Scripts/DeathMenu.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DeathMenu : MonoBehaviour
 {
     public string mainMenuLevel;
     private ScoreManager scoreManager;
+    public Text pointsText;
 
     public void Start()
     {
         scoreManager = FindObjectOfType<ScoreManager>();
+    }
+
+    public void Update()
+    {
+        pointsText.text = "You got " + Mathf.Round(scoreManager.scoreCount) + " points!";
     }
 
     public void RestartGame()
